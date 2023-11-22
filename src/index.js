61051b24b0c0de7989b7191c6841ce3980c1cbd9
66Gramms <meszarosricsi01@gmail.com>
2023-11-22 20:29:39 +0100

    49. Conditional rendering with multiple returns

--- src/index.js
+++ src/index.js
@@ -84,6 +84,8 @@ function Menu() {
 }
 
 function Pizza(props) {
+	if (props.pizzaObj.soldOut) return null;
+
 	return (
 		<li className="pizza">
 			<img src={props.pizzaObj.photoName} alt={props.pizzaObj.name}></img>
@@ -101,6 +103,8 @@ function Footer() {
 	const openHour = 12;
 	const closeHour = 22;
 	const isOpen = hour >= openHour && hour <= closeHour;
+
+	// if (!isOpen) return <p>CLOSED</p>;
 
 	return (
 		<footer className="footer">
